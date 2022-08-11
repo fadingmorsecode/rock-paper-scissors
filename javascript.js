90f6e7f9b2aa434ea8b2ad60b2602228a948648f
Drake Williamson <williamson.drake@outlook.com>
2022-08-11 14:24:25 -0700

    Edit getComputerChoice function

--- javascript.js
+++ javascript.js
@@ -1,56 +1,42 @@
-const rock = "Rock"
-const paper = "Paper"
-const scissors = "Scissors"
-let computerSelection 
-let result 
+const rock = 'Rock';
+const paper = 'Paper';
+const scissors = 'Scissors';
+
+
+// Your game is going to play against the computer. 
+// So begin with a function called getComputerChoice 
+// that will randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’.
+// We’ll use this function in the game to make the computer’s play. 
 
 function getComputerChoice() {
-    let computerChoice = Math.floor(Math.random()*100);
-    if (computerChoice <= 33) {
-      computerSelection = "Rock";
-    } else if (computerChoice <=66) {
-      computerSelection = "Paper";
+    let randomNumber = Math.floor*(Math.random()*100)
+    if (randomNumber < 34) {
+        return rock;
+    } else if (randomNumber < 66) {
+        return paper;
     } else {
-      computerSelection = "Scissors";
+        return scissors; 
     }
 }
 
-getComputerChoice();
-
-console.log(computerSelection);
-
-let playerSelection = prompt("Please enter Rock Paper or Scissors");
-
-function lowerCase() {
-    return result = playerSelection.toLowerCase();
-}
-
-lowerCase();
+console.log("The computer chose " + getComputerChoice());
 
 
-function playRound() {
-  if (result === "rock" && computerSelection === rock) {
-    console.log("It's a tie!")
-  } else if (result === "rock" && computerSelection === paper) {
-    console.log("You lose! Paper beats rock.");
-  } else if (result === "rock" && computerSelection === scissors) {
-    console.log("You Win! Rock beats scissors.");
-  } else if (result === "paper" && computerSelection === paper) {
-    console.log("It's a tie!");
-  } else if (result === "paper" && computerSelection === scissors) {
-    console.log("You lose! Scissors beats paper.");
-  } else if (result === "paper" && computerSelection === rock) {
-    console.log("You win! Paper beats rock.");
-  } else if (result === "scissors" && computerSelection === scissors) {
-    console.log("It's a tie.");
-  } else if (result === "scissors" && computerSelection === paper) {
-    console.log("You win! Scissors beats paper.");
-  } else {
-    console.log("You lose! Rock beats scissors.");
-  } 
-}
-
-playRound();
 
 
- 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
